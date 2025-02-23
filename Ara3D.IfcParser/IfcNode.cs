4850ab77cbdfcd30e1f0252df93e1eefe4a48f0a
Tim Lewis <tim@scai.group>
2025-02-23 17:11:55 +1100

    Add Published property to IfcNode class

--- Ara3D.IfcParser/IfcNode.cs
+++ Ara3D.IfcParser/IfcNode.cs
@@ -4,6 +4,8 @@ namespace Ara3D.IfcParser
 {
     public class IfcNode : IfcEntity
     {
+        public bool Published { get; set; } = false;
+
         public IfcNode(IfcGraph graph, StepInstance lineData)
             : base(graph, lineData)
         {
