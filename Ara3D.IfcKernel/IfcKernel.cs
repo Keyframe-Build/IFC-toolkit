e25b57adf4cc1c4b8021ba60d2d86de5e07998a8
Tim Lewis <tim@scai.group>
2025-03-14 09:25:17 +1100

    Remove unused IfcKernel project files and related resources to streamline the codebase

--- Ara3D.IfcKernel/IfcKernel.cs
+++
@@ -1,6 +0,0 @@
-using Ara3D.IfcUtilityResource;
-
-namespace Ara3D.IfcKernel;
-
-public record IfcRoot(string GlobalId, IfcOwnerHistory OwnerHistory, string Name, string Description);
-
--- Ara3D.IfcKernel/IfcUtilityResource.cs
+++
@@ -1,6 +0,0 @@
-using Ara3D.IfcActorResource;
-
-namespace Ara3D.IfcUtilityResource;
-
-public record IfcApplication(IfcOrganization ApplicationDeveloper, string Version, string ApplicationFullName, string ApplicationIdentifier);
-public record IfcOwnerHistory(IfcPersonAndOrganisation OwningUser, string OwningApplication, DateTime CreationDate);
