a307bb506078b30117948f2aa4a94f25537e6ac9
Tim Lewis <tim@scai.group>
2025-03-18 21:40:34 +1100

    Refactor IfcPropSetRelation class to improve code clarity by commenting out the PropSet property implementation

--- Ara3D.IfcParser/IfcPropSetRelation.cs
+++ Ara3D.IfcParser/IfcPropSetRelation.cs
@@ -8,15 +8,17 @@ public class IfcPropSetRelation : IfcRelation
     public IfcPropSetRelation(IfcGraph graph, StepInstance lineData, StepId from, StepList to)
         : base(graph, lineData, from, to) { }
 
-    public IfcPropSet PropSet
-    {
-        get
+    /*
+        public IfcPropSet PropSet
         {
-            var node = Graph.GetNode(From);
-            var r = node as IfcPropSet;
-            if (r == null)
-                throw new System.Exception($"Expected a property set not {node} from id {From}");
-            return r;
+            get
+            {
+                var node = Graph.GetNode(From);
+                var r = node as IfcPropSet;
+                if (r == null)
+                    throw new System.Exception($"Expected a property set not {node} from id {From}");
+                return r;
+            }
         }
-    }
+    */
 }
