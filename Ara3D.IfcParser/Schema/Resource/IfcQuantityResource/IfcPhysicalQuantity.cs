8b654941461d0a571bb6534503f1f6929592b8af
Tim Lewis <tim@scai.group>
2025-03-18 14:37:20 +1100

    Add IfcQuantity classes for area, count, number, volume, and weight representations

--- Ara3D.IfcParser/Schema/Resource/IfcQuantityResource/IfcPhysicalQuantity.cs
+++ Ara3D.IfcParser/Schema/Resource/IfcQuantityResource/IfcPhysicalQuantity.cs
@@ -19,6 +19,24 @@ public class IfcPhysicalSimpleQuantity : IfcPhysicalQuantity
         : base(graph, lineData) { }
 }
 
+public class IfcQuantityArea : IfcPhysicalSimpleQuantity
+{
+    public double AreaValue => (this[3] as StepNumber).Value;
+    public string? Formula => (this[3] as StepString)?.Value.ToString();
+
+    public IfcQuantityArea(IfcGraph graph, StepInstance lineData)
+        : base(graph, lineData) { }
+}
+
+public class IfcQuantityCount : IfcPhysicalSimpleQuantity
+{
+    public int CountValue => (this[3] as StepInteger).Value;
+    public string? Formula => (this[3] as StepString)?.Value.ToString();
+
+    public IfcQuantityCount(IfcGraph graph, StepInstance lineData)
+        : base(graph, lineData) { }
+}
+
 public class IfcQuantityLength : IfcPhysicalSimpleQuantity
 {
     public double LengthValue => (this[3] as StepNumber).Value;
@@ -28,11 +46,29 @@ public class IfcQuantityLength : IfcPhysicalSimpleQuantity
         : base(graph, lineData) { }
 }
 
-public class IfcQuantityArea : IfcPhysicalSimpleQuantity
+public class IfcQuantityNumber : IfcPhysicalSimpleQuantity
 {
-    public double AreaValue => (this[3] as StepNumber).Value;
+    public double NumberValue => (this[3] as StepNumber).Value;
     public string? Formula => (this[3] as StepString)?.Value.ToString();
 
-    public IfcQuantityArea(IfcGraph graph, StepInstance lineData)
+    public IfcQuantityNumber(IfcGraph graph, StepInstance lineData)
+        : base(graph, lineData) { }
+}
+
+public class IfcQuantityVolume : IfcPhysicalSimpleQuantity
+{
+    public double VolumeValue => (this[3] as StepNumber).Value;
+    public string? Formula => (this[3] as StepString)?.Value.ToString();
+
+    public IfcQuantityVolume(IfcGraph graph, StepInstance lineData)
+        : base(graph, lineData) { }
+}
+
+public class IfcQuantityWeight : IfcPhysicalSimpleQuantity
+{
+    public double WeightValue => (this[3] as StepNumber).Value;
+    public string? Formula => (this[3] as StepString)?.Value.ToString();
+
+    public IfcQuantityWeight(IfcGraph graph, StepInstance lineData)
         : base(graph, lineData) { }
 }
